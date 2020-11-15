8f8626d95fb7748c98d825ca050ead2a23d1a040
DevKeone <keone.somers@outlook.com>
2020-11-15 20:38:48 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kitchenSink.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
